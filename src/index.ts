9109582c6b9d814236dd1a9f616fda86f81f7a20
troy <11104994@vivo.com>
2020-05-25 15:10:59 +0800

    add: main function.

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import asyncHooks from 'async_hooks';
+
+const httpContext = new Map();
+
+const hooks = asyncHooks.createHook({
+  init (id, type, triggerId) {
+    if (httpContext.has(triggerId)) {
+        httpContext.set(id, httpContext.get(triggerId));
+    }
+  },
+  /**
+   * Remove the data
+   */
+  destroy (id) {
+    if (!httpContext.has(id)) {
+      return;
+    }
+    httpContext.delete(id);
+  },
+});
+
+hooks.enable();
+
+export function set (key: any, value: any) {
+  const asyncId = asyncHooks.executionAsyncId();
+  const store = httpContext.get(asyncId)
+  if (store) {
+    store.set(key, value)
+  }
+}
+
+export function get (key: any) {
+  const asyncId = asyncHooks.executionAsyncId();
+  const store = httpContext.get(asyncId)
+  if (store) {
+    store.get(key);
+  }
+}
+
+export function scope () {
+  const asyncId = asyncHooks.executionAsyncId();
+  httpContext.set(asyncId, new Map());
+}
