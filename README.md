8c2387e31c57fbe1ddab12fbd7c0b471550d0bdd
troy <11104994@vivo.com>
2020-11-26 20:12:42 +0800

    update: add information to README.md

--- README.md
+++ README.md
@@ -1,9 +1,63 @@
 # HTTP Context NodeJS
+
 Get and set request-scoped context anywhere, and use it in any framework.
+
+## Installation
+
+It requires node v8.2.1 or higher for ES2015 and async_hooks support.
+
+```bash
+$ npm i http-context-nodejs
+```
+
 ## Quick start
-### TODO
-### Express
-### Nest
-### Koa
+### How to use it in [NestJS](https://nestjs.com/)
+
+#### Set it in the MainJS
+```js
+...
+import * as uuid from 'node-uuid';
+import * as httpContext from 'http-context-nodejs'
+...
+// Use httpContext and set traceId to trace all the logs within a http request
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    httpContext.scope();
+    httpContext.set('traceId', uuid.v1());
+    next();
+  })
+```
+
+#### Use it in anywhere
+
+```js
+import * as httpContext from 'http-context-nodejs'
+...
+    const traceId = httpContext.get('traceId');
+```
+
+### How to use it in ExpressJS
+
+#### Set it in the AppJS
+
+```js
+...
+import * as uuid from 'node-uuid';
+import * as httpContext from 'http-context-nodejs';
+...
+app.use((req, res, next) => {
+    httpContext.scope();
+    httpContext.set('traceId', uuid.v1());
+    next();
+});
+
+```
+### Use it in anywhere
+
+```js
+import * as httpContext from 'http-context-nodejs'
+...
+    const traceId = httpContext.get('traceId');
+```
+
 ## How it works
 ### TODO
--- test/index.ts
+++ test/index.ts
@@ -21,7 +21,7 @@ describe('Http Context', () => {
                 HttpContext.scope();
                 HttpContext.set('test', id)
                 setTimeout(() => {
-                    const test = HttpContext.get('test')
+                    const test = HttpContext.get('test');
                     resolve(test)
                 }, 100)
             })
@@ -30,4 +30,28 @@ describe('Http Context', () => {
         asyncCall(1).then(v => expect(v).to.equal(1))
         asyncCall(2).then(v => expect(v).to.equal(2))
     })
+
+    it('expect work at multiple async resources', () => {
+        HttpContext.scope();
+        HttpContext.set('id', 1);
+        setTimeout(() => {
+            HttpContext.set('id', 2);
+            setImmediate(() => {
+                HttpContext.set('name', 'http-context');
+                process.nextTick(() => {
+                    HttpContext.set('text', 'who am I');
+                    Promise.resolve().then(async () => {
+                        const id = HttpContext.get('id');
+                        const name = HttpContext.get('name');
+                        const text = HttpContext.get('text');
+                        expect(id).to.equal(2);
+                        expect(name).to.equal('http-context');
+                        expect(text).to.equal('who am I');
+
+                    });
+                });
+            });
+        }, 10);
+    })
+      
 });
